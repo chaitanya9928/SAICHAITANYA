f54fd450992c09e3ca521abe5661c7ad6e134ad2
GunsyX <dharanisrivatsa@gmail.com>
2022-03-04 13:26:21 +0530

    init setup

---
+++ readme.md
@@ -0,0 +1,19 @@
+# FRONTEND SETUP FOR EFG MODULE
+
+## This file contains meta info and some notes, checklists, logs etc about the app
+
+### Checklist
+
+- [x] NPM initialize
+- [x] Git Repo initialize
+- [x] Base folder/file structure (app.js, src, gitignore etc)
+- [x] React, SCSS, dev server, HMR, react refresh setup
+- [ ] Manifest.json, favicon
+- [ ] Service worker setup
+- [ ] Cache in service worker setup
+- [ ] Reset styles
+- [ ] Base Styles, scalable unit (REM), media queries
+- [ ] StateHouse setup
+- [ ] Overlay setup
+- [ ] PWA Setup
+- [ ] Proper PWA go-back functioning
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = (_, argv) => {
+    const mode = argv.mode;
+    const isDevelopment = mode === 'development';
+
+    return {
+        mode: 'development',
+        module: {
+            rules: [
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-react', '@babel/preset-env'],
+                            plugins: ['@babel/plugin-transform-runtime', isDevelopment && require('react-refresh/babel')].filter(Boolean),
+                        },
+                    },
+                },
+                {
+                    test: /\.scss$/,
+                    use: ['style-loader', 'css-loader', 'sass-loader'],
+                }
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: 
+                './public/index.html'
+            }),
+            isDevelopment && new ReactRefreshWebpackPlugin(),
+        ].filter(Boolean),
+        devServer: {
+            port: 1000,
+            hot: true
+        },
+    }
+}
